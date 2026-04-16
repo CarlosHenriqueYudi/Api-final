0ed2c83d78e1f7335f49f19e3c907e204ab3949e
CarlosHenriqueYudi <carlosyudi@edu.unifil.br>
2026-04-16 19:43:36 -0300

    Criar database.js

---
+++ database.js
@@ -0,0 +1,24 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./database.db");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS autores (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT NOT NULL
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS livros (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      titulo TEXT NOT NULL,
+      autor_id INTEGER,
+      ano INTEGER,
+      FOREIGN KEY (autor_id) REFERENCES autores(id)
+    )
+  `);
+});
+
+module.exports = db;
